80c9aba45ca90300295332fd78db3f962418bf5f
Habeeb <wateverlahaiya@gmail.com>
2013-02-28 12:18:33 +0800

    attacking mechanism work in progress

--- heroObject.js
+++ heroObject.js
@@ -13,13 +13,19 @@ function heroObject(){
     this.lastRender = Date.now();
     this.animSpeed = 250;
     this.image = new Image();
-    this.whichSprite = 0;
+	//Specifies the type of character
+    this.whichSprite = 0;	
     // How many pixels do we want to move the hero each loop
     this.moveSpeed = 4;
+    this.currentSpriteImageIndex = 0;	//Can use this and * by number of pixels an image is to get the current sprite image
+    this.vectorX = 0;					//Set these during collision detections
+    this.vectorY = 0;
 
-	//salman i edited the this.x to Math.floor(this.x) and same for the this.y..the reason being is that I read from a book that described
-	//that the sprites might have fractional values and if we render with those values, we may see glitches on the canvas..sprites should be
-	//drawn at integer positions only...also it is important when we use start using the physics formulas...
+	 /*
+	 * salman i edited the this.x to Math.floor(this.x) and same for the this.y..the reason being is that I read from a book which described
+	 * that the sprites might have fractional values and if we render with those values, we may see glitches on the canvas..sprites should be
+	 * drawn at integer positions only...also it will be important when we use start using the physics formulas...
+	 */
     this.render = function(){
         context.drawImage(this.image, this.whichSprite, 0, 
             			  this.width, this.height, Math.floor(this.x), 
@@ -92,20 +98,44 @@ function heroObject(){
  * 
  * 
  
+ var maximumHealthLife = 100;
+ var genericAttributes = {
+ 	health: maximumHealthLife
+ };
+ 
  //------------------------ mainCharacter implementation -------------------------
  
  var mainCharacter = function(){
 
+	//Attributes associated with the main character specifically
+	this.arrayOfLastTenMoves = new Array();		//to be used for probability distribution or if same as the keys array then, this can be deemed redundant
+	
+	
+	//Any action done by the sprite be it punch or kick has to be in the standing state facing us
 	this.punch = function() {
-
+		goToStandingStillSprite();
+		//Set the locations of where the new sprite image is to be drawn
+		//Then render
+		this.render();
+		
 	};
 	
 	this.kick = function() {
-
+		goToStandingStillSprite();
 	};	
 	
+	this.goToStandingStillSprite = function() {
+		//this.currentSpriteImage = whatever the array containing the sprite is and whichever index/offset the standing still sprite is located
+		this.render();
+	}
+	
+
  }//end of mainCharacter constructor
- mainCharacter.prototype = new heroObject();	//Inherit heroObject properties...
+ 
+ //Inherit heroObject properties...
+ mainCharacter.prototype = new heroObject();	
+ //add the generic properties..the reason it is done this way, lady sprites i believe do not need health?..thus dont put in the main construtctor
+ mainCharacter.prototype = new genericAttributes();
  
  //------------------------ end of mainCharacter implementation ------------------
  
