853f09d6cfe848001d80fb8f0360b102959be01b
Salman Gadit <salman.gadit@gmail.com>
2013-03-08 03:37:53 +0800

    Included game strategies as JSON objects

---
+++ data/gamePhases.js
@@ -0,0 +1,52 @@
+var gamePhases = [
+    {
+        "phaseName": "A",
+        "scenario": "1:1:1",
+        "wave": "1"
+    },
+    {
+        "phaseName": "B",
+        "scenario": "2",
+        "wave": "1"
+    },
+    {
+        "phaseName": "C",
+        "scenario": "2:1",
+        "wave": "2"
+    },
+    {
+        "phaseName": "D",
+        "scenario": "3",
+        "wave": "2"
+    },
+    {
+        "phaseName": "E",
+        "scenario": "4:2:1",
+        "wave": "3"
+    },
+    {
+        "phaseName": "F",
+        "scenario": "4",
+        "wave": "3"
+    },
+    {
+        "phaseName": "G",
+        "scenario": "2:1",
+        "wave": "4"
+    },
+    {
+        "phaseName": "H",
+        "scenario": "6",
+        "wave": "4"
+    },
+    {
+        "phaseName": "I",
+        "scenario": "4:2:2:1",
+        "wave": "5"
+    },
+    {
+        "phaseName": "J",
+        "scenario": "10",
+        "wave": "5"
+    }
+];
