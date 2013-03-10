092a64378c7af2761ef6538541d6422a9d9bf892
Maximilian Chow Yuan Ing <chowyuaning@gmail.com>
2013-03-10 17:06:30 +0800

    solved minor bug issues
    1 problem:
    -when there's more than 4 bots flocking towards 1 bot (player/girl), if
    the surrounding grids are occupied by collidables/outside of grid
    system, the bots won't know where to go, and will cause a huge lag issue
    in the system
    -in other words, in any situation where the number of bots moving
    towards one point are more than the available spaces directly around 1
    bot, the game goes haywire

--- Controller.js
+++ Controller.js
@@ -5,7 +5,10 @@ function Controller(VG, hero, enemies){
 	}
 	hero.targetBot = null;
 	hero.actionType = 0;
-	if(hero.gridX + 1 <= 32 && hero.gridX - 1 >=0 && hero.gridY + 1 <= 27 && hero.gridY - 1 >= 0){
+	if(	hero.gridX + 1 <= 32 && 
+		hero.gridX - 1 >=0 && 
+		hero.gridY + 1 <= 27 
+		&& hero.gridY - 1 >= 0){
 		if(hero.facingWhichDirection == "up"){
 			if(VG[hero.gridX][hero.gridY-1] != null && (VG[hero.gridX][hero.gridY-1].selfType == 1 || VG[hero.gridX][hero.gridY-1].selfType == 2) ){
 				hero.targetBot = VG[hero.gridX][hero.gridY-1];
@@ -60,67 +63,70 @@ function setGrid(hero, enemies /*, girls*/){
 //instantly breaks off from function if hero is detected
 //only need to handle checking for enemies
 function awareness(bot, VG){
-	//above
-	
-	if(VG[bot.gridX][bot.gridY-1] != null){
-		bot.facingWhichDirection = "up";
-		bot.targetBot = VG[bot.gridX][bot.gridY-1];
-		if(VG[bot.gridX][bot.gridY-1].selfType == 0){		
-			bot.actionType = 2;
-			//console.log("hero up");
-			return;
+	if(	bot.gridX + 1 <= 32 
+		&& bot.gridX - 1 >=0 
+		&& bot.gridY + 1 <= 27 
+		&& bot.gridY - 1 >= 0){
+		//above	
+		if(VG[bot.gridX][bot.gridY-1] != null){
+			bot.facingWhichDirection = "up";
+			bot.targetBot = VG[bot.gridX][bot.gridY-1];
+			if(VG[bot.gridX][bot.gridY-1].selfType == 0){		
+				bot.actionType = 2;
+				//console.log("hero up");
+				return;
+			}
+			if(VG[bot.gridX][bot.gridY-1].selfType >= 3){
+				bot.actionType = 1;
+				VG[bot.gridX][bot.gridY-1].actionType = 1;
+				VG[bot.gridX][bot.gridY-1].facingWhichDirection = "down";
+			}			
 		}
-		if(VG[bot.gridX][bot.gridY-1].selfType >= 3){
-			bot.actionType = 1;
-			VG[bot.gridX][bot.gridY-1].actionType = 1;
-			VG[bot.gridX][bot.gridY-1].facingWhichDirection = "down";
-		}			
-	}
-	//below
-	if(VG[bot.gridX][bot.gridY+1] != null){
-		bot.facingWhichDirection = "down";
-		bot.targetBot = VG[bot.gridX][bot.gridY+1];
-		if(VG[bot.gridX][bot.gridY+1].selfType == 0){			
-			bot.actionType = 2;			
-			//console.log("hero down");
-			return;
+		//below
+		if(VG[bot.gridX][bot.gridY+1] != null){
+			bot.facingWhichDirection = "down";
+			bot.targetBot = VG[bot.gridX][bot.gridY+1];
+			if(VG[bot.gridX][bot.gridY+1].selfType == 0){			
+				bot.actionType = 2;			
+				//console.log("hero down");
+				return;
+			}
+			if(VG[bot.gridX][bot.gridY+1].selfType >= 3){
+				bot.actionType = 1;
+				VG[bot.gridX][bot.gridY+1].actionType = 1;
+				VG[bot.gridX][bot.gridY+1].facingWhichDirection = "up";
+			}
+		}	
+		//left
+		if(VG[bot.gridX-1][bot.gridY] != null){
+			bot.facingWhichDirection = "left";
+			bot.targetBot = VG[bot.gridX-1][bot.gridY];
+			if(VG[bot.gridX-1][bot.gridY].selfType == 0){			
+				bot.actionType = 2;			
+				//console.log("hero left");
+				return;
+			}
+			if(VG[bot.gridX-1][bot.gridY].selfType >= 3){
+				bot.actionType = 1;
+				VG[bot.gridX-1][bot.gridY].actionType = 1;
+				VG[bot.gridX-1][bot.gridY].facingWhichDirection = "right";
+			}
+		}	
+		//right
+		if(VG[bot.gridX+1][bot.gridY] != null){
+			bot.facingWhichDirection = "right";
+			bot.targetBot = VG[bot.gridX+1][bot.gridY];
+			if(VG[bot.gridX+1][bot.gridY].selfType == 0){	
+				bot.actionType = 2;			
+				//console.log("hero right");
+				return;
+			}
+			if(VG[bot.gridX+1][bot.gridY].selfType == 3){
+				bot.actionType = 1;
+				VG[bot.gridX+1][bot.gridY].actionType = 1;
+				VG[bot.gridX+1][bot.gridY].facingWhichDirection = "left";
+			}
+			
 		}
-		if(VG[bot.gridX][bot.gridY+1].selfType >= 3){
-			bot.actionType = 1;
-			VG[bot.gridX][bot.gridY+1].actionType = 1;
-			VG[bot.gridX][bot.gridY+1].facingWhichDirection = "up";
-		}
-	}
-	
-	//left
-	if(VG[bot.gridX-1][bot.gridY] != null){
-		bot.facingWhichDirection = "left";
-		bot.targetBot = VG[bot.gridX-1][bot.gridY];
-		if(VG[bot.gridX-1][bot.gridY].selfType == 0){			
-			bot.actionType = 2;			
-			//console.log("hero left");
-			return;
-		}
-		if(VG[bot.gridX-1][bot.gridY].selfType >= 3){
-			bot.actionType = 1;
-			VG[bot.gridX-1][bot.gridY].actionType = 1;
-			VG[bot.gridX-1][bot.gridY].facingWhichDirection = "right";
-		}
-	}	
-	//right
-	if(VG[bot.gridX+1][bot.gridY] != null){
-		bot.facingWhichDirection = "right";
-		bot.targetBot = VG[bot.gridX+1][bot.gridY];
-		if(VG[bot.gridX+1][bot.gridY].selfType == 0){	
-			bot.actionType = 2;			
-			//console.log("hero right");
-			return;
-		}
-		if(VG[bot.gridX+1][bot.gridY].selfType == 3){
-			bot.actionType = 1;
-			VG[bot.gridX+1][bot.gridY].actionType = 1;
-			VG[bot.gridX+1][bot.gridY].facingWhichDirection = "left";
-		}
-		
 	}
 }
