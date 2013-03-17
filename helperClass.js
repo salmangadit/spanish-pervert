f142cd6a815587ef34b8a8789fc0f706f84b3ed1
Salman Gadit <salman.gadit@gmail.com>
2013-03-18 03:28:16 +0800

    Distance between two points function

--- helperClass.js
+++ helperClass.js
@@ -43,6 +43,10 @@ helperClass.checkIfTwoRectanglesIntersect = function(xCoordOfRect1, yCoordOfRect
 	
 }
 
+helperClass.distanceBetweenTwoPoints = function(x1, y1, x2, y2){
+	return Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2));
+}
+
 
 
 /*Array.prototype.last = function(){
