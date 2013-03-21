c93712dffd47a82c8eadc321c3b37981db618126
Kendrick <lam3ken@hotmail.com>
2013-03-22 07:27:02 +0800

    Updated Learning code

--- data/playerLearningModels.js
+++ data/playerLearningModels.js
@@ -1,22 +1,22 @@
 var playerLearningModels =
 {
 	"1": {
-		"modelTimeKill": "120000",
+		"modelTimeKill": "100000",
 		"bufferTimeKill": "20000",
-		"modelTimeRescue": "100",
-		"bufferTimeRescue": "50"
+		"modelTimeRescue": "60000",
+		"bufferTimeRescue": "10000"
 	},
 	"2": {
-		"modelTimeKill": "150000",
+		"modelTimeKill": "120000",
 		"bufferTimeKill": "20000",
-		"modelTimeRescue": "100",
-		"bufferTimeRescue": "50"
+		"modelTimeRescue": "60000",
+		"bufferTimeRescue": "10000"
 	},
 	"3": {
-		"modelTimeKill": "200000",
+		"modelTimeKill": "150000",
 		"bufferTimeKill": "50000",
-		"modelTimeRescue": "100",
-		"bufferTimeRescue": "50"
+		"modelTimeRescue": "10000",
+		"bufferTimeRescue": "10000"
 	},
 	"4": {
 		"modelTimeKill": "100",
