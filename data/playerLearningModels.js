f6cf177ae0822499ee343c09c52ad5009e0c2dc8
Kendrick <lam3ken@hotmail.com>
2013-03-22 06:45:34 +0800

    MOdel learning

--- data/playerLearningModels.js
+++ data/playerLearningModels.js
@@ -1,20 +1,20 @@
 var playerLearningModels =
 {
 	"1": {
-		"modelTimeKill": "100",
-		"bufferTimeKill": "50",
+		"modelTimeKill": "120000",
+		"bufferTimeKill": "20000",
 		"modelTimeRescue": "100",
 		"bufferTimeRescue": "50"
 	},
 	"2": {
-		"modelTimeKill": "100",
-		"bufferTimeKill": "50",
+		"modelTimeKill": "150000",
+		"bufferTimeKill": "20000",
 		"modelTimeRescue": "100",
 		"bufferTimeRescue": "50"
 	},
 	"3": {
-		"modelTimeKill": "100",
-		"bufferTimeKill": "50",
+		"modelTimeKill": "200000",
+		"bufferTimeKill": "50000",
 		"modelTimeRescue": "100",
 		"bufferTimeRescue": "50"
 	},
